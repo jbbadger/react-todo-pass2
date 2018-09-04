293587ecc48e28f57f95386d9abb8d2c38a02f98
jbmominee <jaymominee@gmail.com>
2018-09-03 19:30:19 -0500

    added state and props

--- react-to-do-pass2/src/App.js
+++ react-to-do-pass2/src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 import ToDo from './component/ToDo.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { description: 'order food', isCompleted: true },
+        { description: 'take out trash', isCompleted: false },
+        { description: 'Buy new dishes', isCompleted: true }
+      ]
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <ul>
-          <ToDo />
-          <ToDo />
+          { this.state.todos.map( (todo, index) =>
+            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+          )}
         </ul>
       </div>
     );
