87ed3692eb5e4fce9e203e794329398cc66fb751
jbmominee <jaymominee@gmail.com>
2018-09-03 19:13:34 -0500

    updated app js file

--- react-to-do-pass2/src/App.js
+++ react-to-do-pass2/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import ToDo from './components/ToDo.js';
+import ToDo from './component/ToDo.js';
 
 class App extends Component {
   render() {
